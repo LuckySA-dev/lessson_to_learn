7c51d7427a7dd8be5e1989f290f8e3b02c3d4542
Sirirod Udomdach <s6806022510271@email.kmutnb.ac.th>
2025-07-24 18:08:24 +0700

    modified:   Lecture-1/assign_2.py

--- Lecture-1/assign_2.py
+++ Lecture-1/assign_2.py
@@ -1,10 +1,8 @@
 def find_sum_pair(my_list:list,number:int):
-    
     for index,n in enumerate(my_list):
         if (my_list[index] + my_list[index+1]) == number:
             return my_list[index], my_list[index+1]
             
-    
 def main():
     my_list = list(map(int, input("Enter Number (space-separate): ").strip().split()))
     number = int(input("What's Number you need to find sum : "))
